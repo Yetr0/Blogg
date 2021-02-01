3c90ff97bfd525a70a63e7ad8552a35d751335e1
Lucas Altesson <lucas.altesson@learnet.se>
2021-02-01 22:59:53 +0100

    finished

--- code.js
+++ code.js
@@ -8,57 +8,78 @@ jag har felsökt på följande sätt:
 var aob = document.getElementById("AmountOfBlogs");
 var numbers = [];
 var nums = "";
-            
+
 
         var posts = [];
 
         for (i = 0; i < 100; i++){
-            var title = document.getElementById("title");
-            var text = document.getElementById("text");
             var article = document.createElement("article");
+            article.id=i;
             var h3 = document.createElement("h3");
             var p = document.createElement("p");
             var button = document.createElement("button");
-                button.onclick = function (){
-                    var title = posts[i].childNodes[1].innerHTML;
-                    var text = posts[i].childNodes[2].innerHTML;
-                    var element = this.parentElement;
-                    var editbtn = document.createElement("button");
-                    editbtn.innerText = "Save";
-                    var editTitle = document.createElement("input");
-                    editTitle.type = "text";
-                    editTitle.value = title;
-                    editTitle.className = "col-12";
-                    var editText = document.createElement("textarea");
-                    editText.value = text;
-                    editText.className = "col-12 no-resize height";
-                    element.innerHTML = "";
-                    element.append(editbtn, editTitle, editText);
+            button.className = "btn btn-light mb-2";
+            button.innerText = "Edit";
+                button.onclick = function () {
+                    Edit(this.parentElement.id);
                 };
             h3.innerText = "Lorem ipsum, dolor sit";
             p.innerText = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam repudiandae quae fugit dolores placeat suscipit similique, rem sequi deleniti totam quidem doloremque autem. Eligendi maxime quod quas, laudantium esse minus!Impedit obcaecati eveniet ducimus vitae nobis minus dolorum voluptates deserunt amet eos, placeat, nulla deleniti magni, velit veniam aliquid. Rem id optio nam dicta aperiam quos consequuntur neque atque voluptatibus?";
             article.append(button, h3, p);
-            article.className = "w-50 m-auto mt-3 mb-5 bg-dark text-light p-4 rounded";
-            article.id=i;
+            article.className = "w-75 m-auto mt-3 mb-5 bg-dark text-light p-4 rounded";
             posts.push(article);
         }
-            console.log(posts);
+
+
+        /* Ändrar knappens text och funktion till Save och Save(), sen tas h3 och p bort för att istället 
+        lägga till ett input och textarea så att man kan ändra texten */
         function Edit(id){
             var post = document.getElementById(id);
-            console.log(id);
+            post.childNodes[0].innerText = "Save";
+            post.className += "row";
+            var title = posts[id].childNodes[1].innerText;
+            var text = posts[id].childNodes[2].innerText;
+            var h3 = document.createElement("input");
+            h3.type = "text";
+            h3.className = "col-12 mb-3"
+            h3.value = title;
+            var p = document.createElement("textarea");
+            p.value = text;
+            p.className = "col-12 height"
+
+            post.childNodes[1].remove();
+            post.childNodes[1].remove(); 
+            post.append(h3, p);
+            post.childNodes[0].onclick = function(){
+                var newtitle = post.childNodes[1].value;
+                var newtext = post.childNodes[2].value;
+                (newtext);
+                Save(post, newtext, newtitle, id);
+            }
+        }
+        /*Här ändras knappens funktion och text så att det står edit och Edit() körs så att man kan redigera inlägget igen. Input och textarea byts ut mot h3 och p igen. */
+        function Save(post, text, titel, id){
+            post.childNodes[1].remove();
+            post.childNodes[1].remove();
+            var h3 = document.createElement("h3");
+            var p = document.createElement("p");
+            h3.innerText = titel;
+            p.innerText = text;
+            post.append(h3,p);
+            post.childNodes[0].innerText = "Edit";
+            post.childNodes[0].onclick = function(){
+                Edit(id);
+            };
+            posts[id] = post;
         }
 
+        /*Efter att användaren ändrat på reglaget uppe i headern så läggs mängden inlägg som den angett till i en section.*/
         function AddPost(amount){
             document.getElementById("bloggs").innerHTML = "";
             for (i = 0; i < amount; i++){
                 var element = document.getElementById("bloggs");
                 element.appendChild(posts[i]);
             }
-        }
-
-        function ClearValues( input1, input2){
-        input1.value = "";
-        input2.value = "";
         }
 
         aob.addEventListener("keydown", function(e){
@@ -73,29 +94,5 @@ var nums = "";
             document.getElementById("value").innerText = aob.value + " inlägg visas";
             AddPost(parseInt(aob.value));
         }
-
-        /*aob.addEventListener("keyup", function(e){
-            if(e.keyCode >= 48 && e.keyCode <= 57){
-                numbers.push(e.key);
-                for(var i = 0; i < numbers.length; i++){
-                    if(nums <= 100)
-                    {
-                        nums += numbers[i];
-                    }
-                }
-                if(nums > 100){
-                    nums = 100;
-                }
-                console.log(nums);
-                document.getElementById("value").innerText = nums;
-                aob.value = nums;
-                console.log(numbers.toString());
-                setTimeout(function(){
-                    numbers = [];
-                    nums = "";
-                    console.log(numbers.toString(), nums);
-                }, 1000)
-            }
-        })*/
 
         
